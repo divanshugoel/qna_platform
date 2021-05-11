6da22aa04b12ed64805d4f93e66f7b10f0dbf341
divanshugoel <divanshu.goel339@gmail.com>
2021-05-11 13:48:04 +0530

    added project directory

---
+++ src/main/java/org/interviewprep/app/qnaplatform/QnaplatformApplication.java
@@ -0,0 +1,13 @@
+package org.interviewprep.app.qnaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/interviewprep/app/qnaplatform/QnaplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.interviewprep.app.qnaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
